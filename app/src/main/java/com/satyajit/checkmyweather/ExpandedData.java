4c82ba1a136dfb0313ae91536cbc06ab51425d10
Satyajit Ghosh <satyajit.edu@outlook.com>
2021-12-23 22:38:36 +0530

    second activity added . Backend tested . UI left

---
+++ app/src/main/java/com/satyajit/checkmyweather/ExpandedData.java
@@ -0,0 +1,14 @@
+package com.satyajit.checkmyweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExpandedData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expanded_data);
+    }
+}
