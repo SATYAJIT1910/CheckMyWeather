9e7db89fecbbc7c4e6edd8b96ed8d1e621093477
Satyajit Ghosh <satyajit.edu@outlook.com>
2021-12-23 23:33:05 +0530

    ui added to second activity

--- app/src/main/java/com/satyajit/checkmyweather/ExpandedData.java
+++ app/src/main/java/com/satyajit/checkmyweather/ExpandedData.java
@@ -2,13 +2,83 @@ package com.satyajit.checkmyweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ExpandedData extends AppCompatActivity {
-
+    private TextView current_temp,area,min_temp,max_temp,humidityView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expanded_data);
+
+        current_temp=findViewById(R.id.current_temp);
+        area=findViewById(R.id.areafield);
+        min_temp=findViewById(R.id.min_temp);
+        max_temp=findViewById(R.id.max_temp);
+        humidityView=findViewById(R.id.humidity);
+
+        // location data received from another activity
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+
+        //API CALL
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&APPID=edf7b9671772635f19f881dc029fbc79";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String current_temperature=response.getJSONObject("main").getString("temp");
+                            current_temperature=Double.toString(Double.parseDouble(current_temperature)-273.15);
+                            current_temp.setText(current_temperature.substring(0,5)+" °C");
+
+                            String min_temperature=response.getJSONObject("main").getString("temp_min");
+                            min_temperature=Double.toString(Double.parseDouble(min_temperature)-273.15);
+                            min_temp.setText("min "+min_temperature.substring(0,5)+" °C");
+
+
+                            String max_temperature=response.getJSONObject("main").getString("temp_max");
+                            max_temperature=Double.toString(Double.parseDouble(max_temperature)-273.15);
+                            max_temp.setText("max "+max_temperature.substring(0,5)+" °C");
+
+                            String humidity=response.getJSONObject("main").getString("humidity");
+                            humidityView.setText("humidity "+humidity);
+
+
+                            String desc=response.getJSONArray("weather").getJSONObject(0).getString("main");
+                            area.setText(desc+" in "+location);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+
+                    }
+                });
+        queue.add(jsonObjectRequest);
+
+
     }
 }
--- app/src/main/java/com/satyajit/checkmyweather/MainActivity.java
+++ app/src/main/java/com/satyajit/checkmyweather/MainActivity.java
@@ -2,13 +2,28 @@ package com.satyajit.checkmyweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
+    private EditText locationText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+        locationText=findViewById(R.id.locationText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,ExpandedData.class);
+                intent.putExtra("location",locationText.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
